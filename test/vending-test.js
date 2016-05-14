7ff543dd8b740ab0f49325f5764bbfed99084f8b
Charles Wentzel <charles@ramparthosting.com>
2016-05-14 16:44:33 -0400

    Add packages mocha/chai, connect with mongo

---
+++ test/vending-test.js
@@ -0,0 +1,17 @@
+var expect = require("chai").expect();
+var app = require("../app.js");
+
+describe("Vending Machine",function(){
+    describe("Coin Acceptance Process",function(){
+        it("checks if penny",function(){
+
+
+        })
+
+
+    })
+
+
+
+
+});
